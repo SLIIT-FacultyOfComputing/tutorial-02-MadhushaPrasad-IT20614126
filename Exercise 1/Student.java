76abdd4444655063d45482e96925c20f0ca17704
MadhushaPrasad <madushaprasad21@gmail.com>
2021-08-10 14:03:02 +0530

    declare marks of three subjects variables

--- Exercise 1/Student.java
+++ Exercise 1/Student.java
@@ -1,5 +1,10 @@
 class Student{
 
+    //create variables for store marks of three subjects
+    private double subject01;
+    private double subject02;
+    private double subject02;
+
     public Student(){
 
     }
