9a00ab8ff80d067459687e39c3a2e7f7c47458f1
MadhushaPrasad <madushaprasad21@gmail.com>
2021-08-10 14:15:18 +0530

    Include a method to display the ID, name and the average marks of the student

--- Exercise 1/Student.java
+++ Exercise 1/Student.java
@@ -3,7 +3,7 @@ class Student{
     //declare a private field for the student's name
     private String studentName;
     //declare a private field for the student's ID number
-    private int studentID;
+    private String studentID;
     //create variables for store marks of three subjects
     private double subject01;
     private double subject02;
@@ -22,6 +22,10 @@ class Student{
         average = (subject01 + subject02 + subject03) / 3;
     }
 
-    
-
+    //display the ID, name and the average marks of the student
+    public void display() {
+        System.out.println("Student Name: " + studentName);
+        System.out.println("Student ID: " + studentID);
+        System.out.println("Average: " + average);
+    }
 }
